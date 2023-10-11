cd8aae86ee484f445a66cbb909d591f7883a8bb1
mokicheck <118475936+mokicheck@users.noreply.github.com>
2023-10-12 01:02:15 +0600

    Update PasswordAuthentication.java

--- PasswordAuthentication.java
+++ PasswordAuthentication.java
@@ -1,8 +1,7 @@
 public class PasswordAuthentication implements AuthenticationStrategy {
     @Override
     public boolean authenticate(String username, String password) {
-        // Здесь можете реализовать аутентификацию по паролю.
-        // Например, сравните переданный пароль с сохраненным паролем пользователя.
-        return true; // Замените на вашу реальную логику
+        
+        return true; 
     }
 }
