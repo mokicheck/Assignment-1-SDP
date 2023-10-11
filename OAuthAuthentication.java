9592442577b73599821f2278f87065f9181441ce
mokicheck <118475936+mokicheck@users.noreply.github.com>
2023-10-12 01:02:33 +0600

    Update OAuthAuthentication.java

--- OAuthAuthentication.java
+++ OAuthAuthentication.java
@@ -1,8 +1,6 @@
 public class OAuthAuthentication implements AuthenticationStrategy {
     @Override
     public boolean authenticate(String username, String oauthToken) {
-        // Здесь можете реализовать аутентификацию с использованием OAuth.
-        // Например, проверьте, действителен ли переданный OAuth-токен.
-        return true; // Замените на вашу реальную логику
+        return true; 
     }
 }
