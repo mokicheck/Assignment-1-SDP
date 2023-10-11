e7da641b73989221a30a0d16e3de4efd61594684
mokicheck <118475936+mokicheck@users.noreply.github.com>
2023-10-12 01:04:59 +0600

    Update AuthenticationExample.java

--- AuthenticationExample.java
+++ AuthenticationExample.java
@@ -4,33 +4,33 @@ public class AuthenticationExample {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        // Шаг 1: Создайте экземпляр UserSingleton и установите имя пользователя
+        // 
         UserSingleton user = UserSingleton.getInstance();
         System.out.print("Введите имя пользователя: ");
         String username = scanner.nextLine();
         user.setUsername(username);
 
-        // Шаг 2: Определите стратегии аутентификации как анонимные классы (лямбда-выражения)
+        // 
         AuthenticationStrategy passwordAuth = (inputUsername, password) -> {
-            // Здесь может быть ваша реализация аутентификации по паролю
-            return true; // Замените на вашу реальную логику
+            
+            return true;
         };
 
         AuthenticationStrategy oauthAuth = (inputUsername, oauthToken) -> {
-            // Здесь может быть ваша реализация аутентификации с использованием OAuth
-            return true; // Замените на вашу реальную логику
+            
+            return true; 
         };
 
-        // Шаг 3: Аутентификация с разными стратегиями
+        //Аутентификация с разными стратегиями
         boolean passwordResult = authenticate(passwordAuth, username, getPasswordFromUser(scanner));
         boolean oauthResult = authenticate(oauthAuth, username, getOAuthTokenFromUser(scanner));
 
-        // Шаг 4: Вывод результатов аутентификации
+        //Вывод результатов аутентификации
         System.out.println("Password authentication result: " + passwordResult);
         System.out.println("OAuth authentication result: " + oauthResult);
     }
 
-    // Шаг 3: Создайте метод для аутентификации с использованием стратегии
+    //  метод для аутентификации с использованием стратегии
     private static boolean authenticate(AuthenticationStrategy strategy, String username, String credentials) {
         return strategy.authenticate(username, credentials);
     }
